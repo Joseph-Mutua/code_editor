a51511553ec9f019d32fd638b3bd19f994ea23e1
Joseph Mutua <Mutuaj793@gmail.com>
2022-04-12 12:13:03 +0300

    Auditing dev dependencies

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -14,12 +14,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCellsRouter = void 0;
 const express_1 = __importDefault(require("express"));
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
+    router.use(express_1.default.json());
+    const fullPath = path_1.default.join(dir, filename);
     router.get("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         // Make sure cell storage file  exists
         // if the file does not exist, add i a default list of cells
         // Reead the file
+        try {
+            const result = yield promises_1.default.readFile(fullPath, { encoding: "utf-8" });
+            res.send(JSON.parse(result));
+        }
+        catch (err) {
+            if (err.code === "ENOENT") {
+                // Add code to create file and add default cells
+                yield promises_1.default.writeFile(fullPath, "[]", "utf-8");
+                res.send([]);
+            }
+            else {
+                throw err;
+            }
+        }
         // Parse a list of cells out of it
         // Send list of celss back to the browser
     }));
@@ -27,7 +45,11 @@ const createCellsRouter = (filename, dir) => {
         // Make sure the file exists
         // If not, create it
         // Take the list of cells from the request obj
+        // serialize them
+        const { cells } = req.body;
         // Write the cells into the file
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+        res.send({ status: "ok" });
     }));
     return router;
 };
